d81e9bffa0341299c5af32f263b423c2c0f5d0ee
Stefan Bauer <sba@gmx.ch>
2021-09-19 11:28:28 +0200

    support multiple toys in argument, seperated by semikolon

--- lupotoy.php
+++ lupotoy.php
@@ -17,17 +17,21 @@ class plgContentLupotoy extends JPlugin
 		    JLoader::import( 'lupo', JPATH_BASE . '/components/com_lupo/models' );
 	    }
 
-		$pattern = '/(\[spiel|spiele|jeu|jeux|gioco|giochi)[\s]{0,4}([0-9]+\.?[0-9]*|\.[0-9]+)(\])/i';
+		//regex matches: [spiel 9007.13;200.2] lorem ipsum [spiel 9000] hallo welt [spiel 10003.3]
+		$pattern = '/(\[spiel|spiele|jeu|jeux|gioco|giochi)[\s]{0,4}([0-9;.]+)(\])/i';
 	    preg_match_all($pattern, $article->text, $matches);
 
 		if(count($matches[0])>0) {
 			$toy_numbers = $matches[2];
 			foreach ($toy_numbers as $toy_number) {
 				$model = new LupoModelLupo();
-				$toy   = $model->getGamesByNumber($toy_number);
-				if ($toy !== false) {
-					$replacement   = '<a href="' . $toy[0]['link'] . '"><img src="' . $toy[0]['image'] . '" title="' . $toy[0]['title'] . '" style="max-width: 200px" ></a>';
-					$pattern = '/(\[spiel|jeux|gioco)[ ]{0,1}('.$toy_number.')(\])/';
+				$toys   = $model->getGamesByNumber($toy_number);
+				if ($toys !== false) {
+					$replacement = '';
+					foreach ($toys as $toy) {
+						$replacement  .= '<a href="' . $toy['link'] . '"><img src="' . $toy['image'] . '" title="' . $toy['title'] . '" style="max-width: 200px" ></a>';
+					}
+					$pattern = '/(\[spiel|spiele|jeu|jeux|gioco|giochi)[ ]{0,4}('.$toy_number.')(\])/';
 					$article->text = preg_replace($pattern, $replacement, $article->text);
 					//$article->text .= '<pre>'. print_r($toy[0],true).'</pre>';
 				}
